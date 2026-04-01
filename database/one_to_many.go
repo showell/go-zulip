4c73929cd91c18ca8f1ea7d0fb3b8566a7f008d3
Steve Howell <showell30@yahoo.com>
2026-04-01 11:58:13 -0400

    wip

---
+++ database/one_to_many.go
@@ -0,0 +1,40 @@
+package database
+
+type IntSet = map[int]struct{}
+
+type OneToMany struct {
+	list_of_sets []IntSet
+}
+
+func NewOneToMany() *OneToMany {
+	return &OneToMany{
+		list_of_sets: make([]IntSet, 0),
+	}
+}
+
+func (self *OneToMany) Update(one_index int, many_index int) {
+	for one_index >= len(self.list_of_sets) {
+		int_set := make(IntSet)
+		self.list_of_sets = append(self.list_of_sets, int_set)
+	}
+
+	many_index_set := self.list_of_sets[one_index]
+	many_index_set[many_index] = struct{}{}
+	self.list_of_sets[one_index] = many_index_set
+}
+
+func (self OneToMany) GetManyIndexesInRandomOrder(one_index int) []int {
+	int_set := self.list_of_sets[one_index]
+
+	lst := make([]int, 0)
+
+	for k := range int_set {
+		lst = append(lst, k)
+	}
+
+	return lst
+}
+
+func (self OneToMany) Count(one_index int) int {
+	return len(self.list_of_sets[one_index])
+}
