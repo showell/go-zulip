0943d03a12b8ae0eb7935409c01cbc8f80b40e93
Steve Howell <showell30@yahoo.com>
2026-04-02 09:05:12 -0400

    wip

--- html/html.go
+++ html/html.go
@@ -15,12 +15,27 @@ type Database = database.Database
 
 func ChannelsHtml(db *Database) string {
 	var sb strings.Builder
+	rows := db.ChannelTable.Rows
 
 	p := sb.WriteString
+	Itoa := strconv.Itoa
 
-	for _, row := range db.ChannelTable.Rows {
-		p(strconv.Itoa(row.Id))
-		p(h.EscapeString(row.Name))
+	p("<h4>")
+	p(Itoa(len(rows)))
+	p(" channels</h4>\n")
+
+	for _, row := range rows {
+		name := h.EscapeString(row.Name)
+		channel_id := Itoa(row.Id)
+		num_topics := Itoa(db.ChannelToAddress.Count(row.Index))
+
+		p("<div class='channel_row'>\n<div class='channel_name'>")
+		p(name)
+		p("</div>\n<div><a href='/topics/")
+		p(channel_id)
+		p(">topic</a></div>\n<div class='channel_count'>")
+		p(num_topics)
+		p("</div>\n</div>\n")
 		p("\n")
 	}
 
